013234a41799599241f1ede037ed09a47020e38f
Christoph Wempe <christoph@wempe.net>
2016-11-04 21:38:30 +0100

    added timeout to sql to prevent DB-lock error

--- award_playlists.sh
+++ award_playlists.sh
@@ -417,7 +417,7 @@ if [ $NOMINEESCOUNT -eq 0 ]
       ID=`echo $LINE | awk '{print $2}'`
       TITLE=`echo $LINE | cut -c 13-`
       # Search title in Database using IMDBid
-      SQLRESULT=`sqlite3 $DBFILE "SELECT c00, playCount, '"$NOMINATIONS"' as nominations FROM movie_view WHERE uniqueid_value IS '"$ID"' AND uniqueid_type IS 'imdb' GROUP BY c00 LIMIT 1"`
+      SQLRESULT=`sqlite3 -init <(echo .timeout $DBTIMEOUT) $DBFILE "SELECT c00, playCount, '"$NOMINATIONS"' as nominations FROM movie_view WHERE uniqueid_value IS '"$ID"' AND uniqueid_type IS 'imdb' GROUP BY c00 LIMIT 1"`
       TITLESQL=`echo $TITLE | sed 's/&/%/g'`
 
       if [ "$SQLRESULT" != "" ]
