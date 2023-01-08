fbb19d10fca422f85840f6c33475f863df9565fa
Christoph Wempe <christoph@wempe.net>
2023-01-08 21:48:33 +0100

    Integrate create_index to main script

--- award_playlists.sh
+++ award_playlists.sh
@@ -238,6 +238,8 @@ fi
 # shellcheck disable=SC1091,SC1090
 source "$CONFIG"
 
+INDEXHTML="${HTMLDIR}/index.html"
+
 # change dir to $BINDIR for git to work
 cd "$BINDIR" || { echo "Command cd failed!"; exit 1; }
 
@@ -270,6 +272,8 @@ if [ "$VERBOSE" -eq 1 ]
     echo -e " BINDIR:         $BINDIR"
     echo -e " CONFIG:         $CONFIG"
     echo -e " DBFILE:         $DBFILE"
+    echo -e " HTMLDIRL:       $HTMLDIR"
+    echo -e " INDEXHTML:      $INDEXHTML"
     echo -e " NOMINEEURL:     $NOMINEEURL"
     echo -e " NOMINEEHTML:    $NOMINEEHTML"
     echo -e " NOMINEEJSON:    $NOMINEEJSON"
@@ -1063,6 +1067,122 @@ if [ "$MAIL" != "" ]
         fi
     fi
 fi
+
+
+####
+# create index.html
+####
+
+# Search for Award-HTML files
+if [ "$VERBOSE" -eq 1 ]
+  then
+    echo ""
+    echo "HTML files:"
+    find ${HTMLDIR} -name nominees*.html
+fi
+
+# Get all available years
+YEARS=$(find ${HTMLDIR} -name nominees*.html | awk -F "_" '{print substr($(NF), 1, length($(NF)-1))}' | sort -u -r)
+#' fix wrong syntax highlighting in mcedit
+
+if [ "$VERBOSE" -eq 1 ]
+  then
+    echo ""
+    echo "YEARS: ${YEARS}"
+fi
+
+
+
+# Get all available events
+EVENTS=$(find ${HTMLDIR} -name nominees*.html | awk -F "_" '{print $(NF-1)}' | sort -u)
+#' fix wrong syntax highlighting in mcedit
+
+if [ "$VERBOSE" -eq 1 ]
+  then
+    echo ""
+    echo "EVENTS: ${EVENTS}"
+fi
+
+
+# Create header of index.html
+{
+   echo -e "<!DOCTYPE html>"
+   echo -e "<html lang=\"en\">"
+   echo -e "  <head>"
+   echo -e "    <meta charset=\"utf-8\"/>"
+   if [[ "${CSSFILE}" == "awards_dark.css" ]]
+     then
+       echo -e "    <meta name=\"theme-color\" content=\"#212121\">"
+   fi
+   echo -e "    <meta name=\"viewport\" content=\"width=device-width\">"
+   echo -e "    <title>Award Shows</title>"
+   echo -e "    <link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"favicons/awards.ico\" />"
+   echo -e "    <link rel=\"stylesheet\" type=\"text/css\" href=\"$CSSFILE\" />"
+   echo -e "    <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\" integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\" />"
+   echo -e "    <script src=\"sorttable.js\"></script>"
+   echo -e "  </head>"
+   echo -e "  <body>"
+   echo -e "    <h1>Award Shows</h1>"
+#   echo -e "    <h2>Award Shows</h2>"
+   echo -e "    <table class=\"sortable shows\">"
+   echo -e "      <thead>"
+   echo -e "        <tr>"
+   echo -e "          <th title=\"Event\" class=\"\">Event</th>"
+   for YEAR in ${YEARS}; do
+     echo -e "          <th title=\"${YEAR}\" class=\"sorttable_nosort\">${YEAR}</th>"
+   done
+   echo -e "        </tr>"
+   echo -e "      </thead>"
+   echo -e "      <tbody>"
+ } >  "$INDEXHTML"
+
+
+# Create body of index table
+for EVENT in ${EVENTS}; do
+  echo -e  "        <tr>"                                                                >> "${INDEXHTML}"
+  echo -e  "          <td title=\"${EVENT}\"        class=\"\">${EVENT}</td>"            >> "${INDEXHTML}"
+  for YEAR in ${YEARS}; do
+    NOMINEEHTML="${HTMLDIR}/nominees_${EVENT}_${YEAR}.html"
+    if [ -f "${NOMINEEHTML}" ]
+      then
+        echo -e  "          <td title=\"${EVENT} ${YEAR}\"  class=\"\"><a href=\"./nominees_${EVENT}_${YEAR}.html\">${YEAR}</a></td>"   >> "${INDEXHTML}"
+      else
+        echo -e  "          <td title=\"no data\"  class=\"\"></td>"                     >> "${INDEXHTML}"
+    fi
+  done
+  echo -e  "        </tr>"                                                               >> "${INDEXHTML}"
+done
+
+
+# Create footer for HTML-file
+{
+  echo -e  "      </tbody>"
+#  echo -e  "      <tfoot>"
+#  echo -e  "        <tr>"
+#  echo -en "          <td></td><td></td><td>"
+#  echo -en           "</td><td></td><td></td><td></td><td></td><td></td>"
+#  echo -e  "        </tr>"
+#  echo -e  "      </tfoot>"
+  echo -e  "    </table>"
+
+  echo -e  "    <table class=\"meta\">"
+  echo -e  "      <tr><td><i class=\"fas fa-sync-alt\"></td>"
+  echo -e  "          <td></i>$DATETIME</td></tr>"
+  echo -e  "      <tr><td><i class=\"fas fa-code-branch\"></i></td>"
+  echo -e  "          <td>${GITCOMMIT}</td></tr>"
+  echo -e  "     <table>"
+  echo -e  "  </body>"
+  echo -e  "</html>"
+} >> "${INDEXHTML}"
+
+
+
+
+
+
+
+
+
 
 
 
