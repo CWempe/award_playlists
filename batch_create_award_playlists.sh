98a9a29ebaef053dc0a92f695eb01eab4ae9abbe
Christoph Wempe <christoph@wempe.net>
2023-01-09 00:56:22 +0100

    Add batch_create_award_playlists.sh

---
+++ batch_create_award_playlists.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# This script creates mutiple award_plalists in one run
+#
+# Usage: ./batch_create_award_playlists.sh FISRTYEAR LASTYEAR "award show letters in quotes"
+# Example: ./batch_create_award_playlists.sh 2010 2020 "a g e r"
+
+echo "first year: $1"
+echo "last year: $2"
+echo "Events: $3"
+
+
+for EVENT in $3
+  do
+    for YEAR in $(seq $1 $2)
+      do
+        echo "$EVENT $YEAR"
+        /storage/scripts/award_playlists/award_playlists.sh -e $EVENT -y $YEAR -x
+    done
+done
+
